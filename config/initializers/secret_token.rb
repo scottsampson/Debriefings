c1297052932687c867a600ff42f5f79cd3902ead
Scott <scott@cloudspace.com>
2011-02-18 12:08:44 -0500

    Initial commit to debriefings

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Debriefings::Application.config.secret_token = 'd0e6e4838534172fafafa9d13ed1fe584075bbeb098603eeb0095fbc023c34af45f5ae185d8ee27c592d1eb5eabf4af468b84e094b29604ba2bbfd602669acca'
