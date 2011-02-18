22ba4f338b4d3f5afcf443be94487615b8bb4316
Scott <scott@cloudspace.com>
2011-02-18 12:24:50 -0500

    Adding deploy.rb file

--- Gemfile
+++ Gemfile
@@ -11,7 +11,7 @@ gem 'mysql2'
 # gem 'unicorn'
 
 # Deploy with Capistrano
-# gem 'capistrano'
+gem 'capistrano'
 
 # To use debugger
 # gem 'ruby-debug'
---
+++ config/deploy.rb
@@ -0,0 +1,58 @@
+set :application, "debriefings"
+set :repository,  "git@github.com:scottsampson/Debriefings.git"
+
+set :user, "root"
+
+set :deploy_to, "/srv/#{application}"
+
+set :deploy_via, :remote_cache
+default_run_options[:pty] = true 
+
+set :ssh_options, { :forward_agent => true, :user => "ubuntu" }
+
+# load 'deploy'
+
+
+# There are also new utility libaries shipped with the core these 
+# include the following, please see individual files for more
+# documentation, or run `cap -vT` with the following lines commented
+# out to see what they make available.
+
+# load 'ext/spinner.rb'              # Designed for use with script/spin
+# load 'ext/passenger-mod-rails.rb'  # Restart task for use with mod_rails
+# load 'ext/web-disable-enable.rb'   # Gives you web:disable and web:enable
+
+set :scm, :git
+
+role :web, "oops.cloudspace.com"
+role :app, "oops.cloudspace.com"
+role :db, "oops.cloudspace.com", :primary => true
+
+task :chmod_all do
+  run "chmod -R 777 #{current_path}/public"
+end
+
+namespace :deploy do
+  task :restart do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+end
+
+task :reset_permissions do
+  run "sudo chown -R ubuntu:ubuntu /srv/#{application}"
+end
+
+namespace :apache do
+  task :restart do
+    run "service apache2 restart"
+  end
+end
+
+# task :copy_old_production do
+#   run "cp -f #{previous_release}/config/environments/production.rb #{current_path}/config/environments/production.rb"
+# end
+
+after :deploy do
+  #copy_old_production
+  chmod_all
+end
